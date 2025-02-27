1c52d881bdbc6ca91f60641efe3794d052e281ee
Victor R. Silva <victorr.silva005@gmail.com>
2025-02-27 17:12:03 -0300

    Programa finalizado

---
+++ verifica-triangulo/CalculadoraTriangulo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verifica_triangulo
+{
+    //Victor Rafael da Silva
+    static class CalculadoraTriangulo
+    {
+        public static bool ValidaTriangulo(List<float> floats)
+        {
+            float x = floats[0];
+            float y = floats[1];
+            float z = floats[2];
+
+            float somaXY = x + y;
+            float somaXZ = x + z;
+            float somaYZ = y + z;
+
+            if (somaXY > z && somaXZ > y && somaYZ > x)
+                return true;
+            else
+                return false;
+        }
+
+        public static string TipoTriangulo(List<float> floats)
+        {
+            float x = floats[0];
+            float y = floats[1];
+            float z = floats[2];
+
+            bool AllEqual = floats.Distinct().Count() == 1;
+            bool AllDifferent = floats.Distinct().Count() == 3;
+
+            if (AllEqual == true)
+                return "Equilátero";
+            else if (x == y || x == z || z == y)
+                return "Isóceles";
+            else if (AllDifferent == true)
+                return "Escaleno";
+
+                return " ";
+        }
+    }
+}
--- verifica-triangulo/Program.cs
+++ verifica-triangulo/Program.cs
@@ -2,20 +2,33 @@
 
 namespace verifica_triangulo
 {
+    //Victor Rafael da Silva
     internal class Program
     {
         static void Main(string[] args)
         {
             byte selector = default;
+            float result = default;
 
-            while (selector != 2){
+            while (selector != 2)
+            {
+                Console.Clear();
                 Console.WriteLine("1- Verificar triângulo \n2- Sair");
-                selector = byte.Parse(Console.ReadLine());
+                Console.Write("\n> ");
+                string auxSelector = Console.ReadLine();
 
-                if (selector != 2)
+                while(!byte.TryParse(auxSelector, out selector) || selector < 1 || selector > 2)
+                {
+                    Console.Write("Valor inválido, tente novamente: ");
+                    auxSelector = Console.ReadLine();
+                }
+
+                Console.Clear();
+
+                if (selector == 1)
                 {
                     List<float> sides = new List<float>();
-                    float result = default;
+                    
 
                     for (int i = 0; i < 3; i++)
                     {
@@ -29,10 +42,16 @@ namespace verifica_triangulo
                         }
                         sides.Add(result);
                     }
-                    foreach (float side in sides)
+                    if (CalculadoraTriangulo.ValidaTriangulo(sides) == true)
                     {
-                        Console.WriteLine(side);
+                        Console.WriteLine(CalculadoraTriangulo.TipoTriangulo(sides));
                     }
+                    else
+                    {
+                        Console.WriteLine("\nTriângulo inválido");
+                    }
+                    Console.Write("\nDigite qualquer tecla: ");
+                    Console.ReadKey();
                 }
             }
             Console.WriteLine("Fechando aplicação...");
