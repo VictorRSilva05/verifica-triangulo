5ff78ecdd2cd3ad6564ecdb194bd9a56b2f89560
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-18 15:39:40 -0300

    Implementada versão inicial com menu

--- verifica-triangulo/Program.cs
+++ verifica-triangulo/Program.cs
@@ -7,54 +7,38 @@ namespace verifica_triangulo
     {
         static void Main(string[] args)
         {
-            byte selector = default;
-            float result = default;
-
-            while (selector != 2)
+            while (true)
             {
                 Console.Clear();
-                Console.WriteLine("1- Verificar triângulo \n2- Sair");
-                Console.Write("\n> ");
-                string auxSelector = Console.ReadLine();
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Classsificador de triângulos");
+                Console.WriteLine("-----------------------------------------");
 
-                while(!byte.TryParse(auxSelector, out selector) || selector < 1 || selector > 2)
-                {
-                    Console.Write("Valor inválido, tente novamente: ");
-                    auxSelector = Console.ReadLine();
-                }
+                Console.Write("Informe o valor do lado X: ");
+                int ladoX = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Informe o valor do lado X: ");
+                int ladoY = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Informe o valor do lado X: ");
+                int ladoZ = Convert.ToInt32(Console.ReadLine());
 
                 Console.Clear();
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Valores informados: ");
+                Console.WriteLine("Lado X: " + ladoX);
+                Console.WriteLine("Valores informados: ");
+                Console.WriteLine("Lado Y: " + ladoY);
+                Console.WriteLine("Valores informados: ");
+                Console.WriteLine("Lado Z: " + ladoZ);
+                
+                Console.WriteLine("-----------------------------------------");
+                Console.WriteLine("Deseja continuar? (s/N) ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
 
-                if (selector == 1)
-                {
-                    List<float> sides = new List<float>();
-                    
-
-                    for (int i = 0; i < 3; i++)
-                    {
-                        Console.Write($"Insira o {i + 1} número: ");
-                        string input = Console.ReadLine();
-
-                        while (!float.TryParse(input, out result) || result < 0)
-                        {
-                            Console.Write("Valor inválido, tente novamente: ");
-                            input = Console.ReadLine();
-                        }
-                        sides.Add(result);
-                    }
-                    if (CalculadoraTriangulo.ValidaTriangulo(sides) == true)
-                    {
-                        Console.WriteLine(CalculadoraTriangulo.TipoTriangulo(sides));
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nTriângulo inválido");
-                    }
-                    Console.Write("\nDigite qualquer tecla: ");
-                    Console.ReadKey();
-                }
+                if( opcaoContinuar != "S")
+                    break;
             }
-            Console.WriteLine("Fechando aplicação...");
         }
     }
 }
